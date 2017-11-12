cb69f829c4f7b6e4cffa852ac4e4b32e97a0ebe0
gabiconde <30847787+gabiconde@users.noreply.github.com>
2017-11-12 20:35:40 -0200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+class UsuarioManager(BaseUserManager):
+    use_in_migrations = True
+    def _create_user(self, ra, password, **extra_fields):
+        if not ra:
+            raise ValueError('RA precisa ser preenchido')
+        user = self.model(ra=ra, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, ra, password=None, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
+    def create_superuser(self, ra, password, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
+
+class Usuario(models.Model):
+    
+    nome = models.CharField(max_length=50)
+    ra = models.IntegerField(unique=True)
+    password = models.CharField(max_length=150)
+    email = models.EmailField()
+    perfil = models.CharField(max_length=1, default='C')
+    ativo = models.BooleanField(default=True)
+    is_anonymous = models.BooleanField()
+    is_authenticated = models.BooleanField()
+
+    USERNAME_FIELD = 'ra'
+    REQUIRED_FIELDS = ['nome']
+
+    objects = UsuarioManager()
+    @property
+    def is_staff(self):
+        return self.perfil == 'C'
+           
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    def get_short_name(self):
+        return self.nome
+    def get_full_name(self):
+        return self.nome
+
+class Aluno(Usuario):
+    curso = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+    
+
