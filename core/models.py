31cbd557d7708aff5fe8e19fa42b85582f5e7889
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2017-11-13 22:20:33 -0200

    Add files via upload

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-#TAMO FUDIDO
+class Usuario(models.Model):
+    nome = models.CharField(max_length = 50)
+    ra = models.IntegerField(unique = True)
+    password = models.CharField(max_length = 150)
+    email = models.EmailField()
+    perfil = models.CharField(max_length = 1)
+    ativo = models.BooleanField(default = True)
